866f7e084631ab3088dd7c7449f365017872a1ab
Mark Tennyson <46404058+marktennyson@users.noreply.github.com>
2023-05-07 16:58:17 +0530

    added gunicorn config

---
+++ gunicorn.conf.py
@@ -0,0 +1,6 @@
+import multiprocessing
+
+bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+errorlog = "/var/log/gunicorn/error.log"
+accesslog = "/var/log/gunicorn/access.log"
